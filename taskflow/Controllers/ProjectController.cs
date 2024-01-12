b4ea574ee31a6841714205eeabff6ad243accd0b
oluwafemi <oluwafemioluwatuyi8@gmail.com>
2024-01-12 14:26:53 +0100

    Project in progress, about to work in the ProjectController

---
+++ taskflow/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace taskflow.Controllers
+{
+    public class ProjectController
+    {
+        
+    }
+}
--- taskflow/Repositories/Implementations/ProjectRepository.cs
+++ taskflow/Repositories/Implementations/ProjectRepository.cs
@@ -5,44 +5,66 @@ using System.Threading.Tasks;
 using taskflow.Repositories.Interfaces;
 using taskflow.Data;
 using taskflow.Models.Domain;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace taskflow.Repositories.Implementations
 {
     public class ProjectRepository(TaskFlowDbContext dbContext) : IProjectRepository
     {
-        public async Task<Project> CreatAsync(Project project)
+        public async Task<Project> CreateAsync(Project project)
         {
-            await dbContext.Workspaces.AddAsync(project);
+            var createProject = await dbContext.Projects.AddAsync(project);
             await dbContext.SaveChangesAsync();
-            return project;
+            return project;       
+        }
+        public async Task<Project> Delete(Guid id)
+        {
+            var DeleteProjectById = await dbContext.Projects.FirstOrDefaultAsync(x => x.Id == id);
+            {
+                if (DeleteProjectById is null)
+                {
+                    return null;
+                }
 
+                dbContext.Projects.Remove(DeleteProjectById);
+                await dbContext.SaveChangesAsync();
+                return DeleteProjectById;
+            }     
+        }
+
+        public Task<ICollection<Project>> FindAllAsync()
+        {     
+            throw new NotImplementedException();
         }
 
         public async Task<Project> ShowAsync(Guid id)
         {
-
-        }
-
-        public async async Task<ICollection<Project>> FindAllAsync()
-
-        {
-
+            var showId = await dbContext.Projects.FirstOrDefaultAsync(x => x.Id == id);
+            if(showId != null)
+            {
+                return showId;
+            }
+            else
+            {
+                return null;
+            }  
         }
 
         public async Task<Project> UpdateAsync(Guid id, Project project)
         {
+            var updateProject = await dbContext.Projects.FirstOrDefaultAsync(x => x.Id == id);
+            if (updateProject is null)
+            {
+                return null;
+            }
+            updateProject.Name = project.Name;
+            updateProject.Description = project.Description;
 
+           await dbContext.SaveChangesAsync();
+
+           return updateProject;
+            
         }
-
-         public async Task<Project> Delete(Guid id)
-         {
-
-         }
-        
-
-       
-        
-
-        
     }
 }
