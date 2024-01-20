f5de10467a7da40db0748dac331ef52fb13136a9
oluwafemi <oluwafemioluwatuyi8@gmail.com>
2024-01-20 11:36:17 +0100

    ProjectTask in progress

--- taskflow/Controllers/ProjectTaskController.cs
+++ taskflow/Controllers/ProjectTaskController.cs
@@ -28,56 +28,9 @@ namespace taskflow.Controllers
         [Authorize]
         [ValidateModel]
         public async Task<IActionResult> CreateAsync(
-           [FromRoute] Guid projectId, Guid workspaceId,
+           [FromRoute] Guid projectId,
            [FromBody] ProjectTaskRequestDto projectTaskRequestDto
            )
-        {
-            // Get the authenticated user's ID
-            var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
-            var user = await userRepository.findByEmail(userEmail);
-            if (user == null)
-                return NotFound(ApiResponse.NotFoundException($"{userEmail}"));
-
-            // Check if the workspace belongs to the user (you might have a different logic for this)
-            var workspace = await workspaceRepository.ShowAsync(workspaceId);
-
-            if (workspace == null || workspace.User?.Email != userEmail)
-                return NotFound(ApiResponse
-                    .NotFoundException($"Workspace not found with the id: {workspaceId}, or does not belong to the user"));
-
-
-            // check if each of the project exists
-            var projectTask = await projectRepository.ShowAsync(workspace, projectId);
-            if (projectTask == null)
-                return NotFound(ApiResponse.NotFoundException($"Project not found with the " +
-                    $"id:[{projectId}], or does not belong to the workspace."));
-
-
-            // Create a new instance of the model from the Dto
-            var projectModel = new ProjectTask
-            {
-                Name = projectTaskRequestDto.Name,
-                Description = projectTaskRequestDto.Description,
-                StartDate = (DateTime)projectTaskRequestDto.StartDate,
-                EndDate = (DateTime)projectTaskRequestDto.EndDate,
-               
-            };
-
-            // Save the model using the repository class
-            await projectTaskRepository.CreateAsync(projectModel);
-
-            return Ok(ApiResponse.SuccessMessageWithData(mapper.Map<List<ProjectTaskResponseDto>>(projectTask)));
-
-
-
-
-
-        }
-
-        [HttpGet]
-        [Authorize]
-        [Route("{id:Guid}")]
-        public async Task<IActionResult> Show([FromRoute] Guid projectId, [FromRoute] Guid id)
         {
             // Get the authenticated user's ID
             var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
@@ -89,22 +42,26 @@ namespace taskflow.Controllers
             var project = await projectRepository.FindById(projectId);
             if (project == null)
                 return NotFound(ApiResponse.NotFoundException($"Project not found with the " +
-                                                              $"id:[{projectId}]"));
+                    $"id:[{projectId}], or does not belong to the workspace."));
+              
+            // Create a new instance of the model from the Dto
+            var projectModel = new ProjectTask
+            {
+                Name = projectTaskRequestDto.Name,
+                Description = projectTaskRequestDto.Description,
+               
+            };
 
-            var workspace = await workspaceRepository.ShowAsync(project.Workspace.Id);
+            // Save the model using the repository class
+            await projectTaskRepository.CreateAsync(projectModel);
 
-            // Check if the user is already added to the project or the owner of thew workspace.
-           // var userExist = await projectMemberRepository.ShowAsync(project, Guid.Parse(user.Id));
-           // if ((userExist == null) && workspace.User?.Email != userEmail)
-            //    return Unauthorized(ApiResponse.AuthorizationException("The current user is not a member of the project"));
+            return Ok(ApiResponse.SuccessMessageWithData(mapper.Map<List<ProjectTaskResponseDto>>(projectModel)));
 
 
-            // fetch the project task
-            var task = await projectTaskRepository.ShowAsync(project, id);
 
-            // Send the response back to user containing the created model.
-            return Ok(ApiResponse.SuccessMessageWithData(mapper.Map<ProjectTaskResponseDto>(task)));
         }
+
+        
     }
 
 }
--- taskflow/Models/DTO/Request/ProjectTaskRequestDto.cs
+++ taskflow/Models/DTO/Request/ProjectTaskRequestDto.cs
@@ -4,22 +4,13 @@ namespace taskflow.Models.DTO.Request
 {
     public class ProjectTaskRequestDto
     {
-        [Required(ErrorMessage = "Name is required")]
-        [StringLength(255, ErrorMessage = "Only a maximum of 255 characters is allowed")]
+        [Required]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Description is required")]
-        [StringLength(500, ErrorMessage = "Only a maximum of 500 characters is allowed")]
+
+        [Required]
+        [DataType(DataType.Text)]
+        [MaxLength(500, ErrorMessage = "Only Max of 500 characters is allowed")]
         public string Description { get; set; }
-
-        [Required(ErrorMessage = "Start date is required")]
-        [DataType(DataType.DateTime, ErrorMessage = "Invalid date format for StartDate. Please use the format yyyy-MM-dd.")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime? StartDate { get; set; }
-
-        [Required(ErrorMessage = "End date is required")]
-        [DataType(DataType.Date, ErrorMessage = "Invalid date format for EndDate. Please use the format yyyy-MM-dd.")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime? EndDate { get; set; }
     }
 }
